c79dcba57a0b6cf8a12087762602ed8e1ca98a11
Pluto-Jin <50690110+Pluto-Jin@users.noreply.github.com>
2019-10-24 17:54:11 +0800

    Add files via upload

---
+++ Classes/Cineplex(incomplete).java
@@ -0,0 +1,36 @@
+// i haven't finished both classes 
+// sorry ...
+import java.util.*
+public class Cineplex
+{
+	private String name;
+	private String location;
+	private Cinema[] cinemas;
+	private Movie[] movies;
+	//movie schedule
+	
+	public Cineplex()
+	{}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getLocation()
+	{
+		return location;
+	}
+
+	public Cinema[] getCinemas()
+	{
+		return cinemas;
+	}
+
+	public Movie[] getMovies()
+	{
+		return movies;
+	}
+
+
+}
