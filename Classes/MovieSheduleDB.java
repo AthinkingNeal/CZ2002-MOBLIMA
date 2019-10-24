9c2f5535b9f9e932ef689db8466aea3d94a73c72
SarahChane98 <47375145+SarahChane98@users.noreply.github.com>
2019-10-24 21:48:55 +0800

    Add files via upload

---
+++ Classes/MovieSheduleDB.java
@@ -0,0 +1,273 @@
+import java.io.*;
+import java.util.*;
+
+public class MovieSheduleDB {
+
+    // integer is the index that is used to identify the
+    private Map<Integer, movieRecord> movieRecords = new HashMap<Integer, movieRecord>();
+
+    /**
+     * Initialise with empty movie records
+     */
+    public MovieSheduleDB() {
+    }
+
+    /**
+     * Initialise with a single record
+     *
+     * @param record an instance of the movieRecord object
+     */
+    public MovieSheduleDB(movieRecord record) {
+        addMovieRecord(record);
+    }
+
+    /**
+     * Initialise with a file containing all the records info
+     *
+     * @param filename file containing records in the order (cineplexID, cinemaID, movieID, movieName, date, startTime, endTime)
+     */
+    public MovieSheduleDB(String filename) {
+        addMovieRecords(filename);
+    }
+
+
+    public void addMovieRecords(int cineplexID, int cinemaID, int movieID, String movieName, String date, String startTime, String endTime) {
+        this.movieRecords.put(movieRecords.size() + 1, new movieRecord(cineplexID, cinemaID, movieID, movieName, date, startTime, endTime));
+    }
+
+    public void addMovieRecord(movieRecord record) {
+        movieRecords.put(movieRecords.size() + 1, record);
+    }
+
+
+    public void addMovieRecords(String fileName) {
+        try {
+            FileReader frStream = new FileReader(fileName);
+            BufferedReader brStream = new BufferedReader(frStream);
+            String inputLine;
+            while ((inputLine = brStream.readLine()) != null) {
+
+                String delims = "[ ]+";
+                String[] tokens = inputLine.split(delims);
+                this.movieRecords.put(movieRecords.size() + 1, new movieRecord(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),
+                        Integer.parseInt(tokens[2]), tokens[3], tokens[4], tokens[5], tokens[6]));
+            }
+            brStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening the input file!");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("IO Error!");
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void displayAllMovieRecords() {
+        for (int i = 0; i < movieRecords.size(); i++) {
+            System.out.println("Index: " + i + 1);
+            movieRecords.get(i).displayMovieRecord();
+        }
+    }
+
+    /**
+     * modify the attributes in the record that has key [index]
+     *
+     * @param index record index that is used to search and identify the record
+     */
+
+    public void modifyMovieReord(int index) {
+        movieRecord newRecord = movieRecords.get(index);
+        System.out.println("Which attribute in the record would you like to change?\n"
+                + "1. Change Movie ID\n" + "2. Change Cineplex ID\n" + "3. Change Cinema ID\n" + "4. Change date of screening\n"
+                + "5. Change start time of screening\n" + "6. Change end time of screening\n" + "7. Quit\n" + "Please key in your option");
+        Scanner s = new Scanner(System.in);
+        int choice = s.nextInt();
+        while (choice != 7) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Please key in the new movie ID");
+                    int newMovieID = s.nextInt();
+                    newRecord.setMovieID(newMovieID);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                case 2:
+                    System.out.println("Please key in the new Cineplex ID");
+                    int newCineplexID = s.nextInt();
+                    newRecord.setCineplexID(newCineplexID);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                case 3:
+                    System.out.println("Please key in the new Cinema ID");
+                    int newCinemaID = s.nextInt();
+                    newRecord.setCinemaID(newCinemaID);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                case 4:
+                    System.out.println("Please key in the new date of screening");
+                    String newdate = s.next();
+                    newRecord.setDateTime(newdate);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                case 5:
+                    System.out.println("Please key in the new start time");
+                    String starttime = s.next();
+                    newRecord.setStartTime(starttime);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                case 6:
+                    System.out.println("Please key in the new end time");
+                    String end = s.next();
+                    newRecord.setEndTime(end);
+                    movieRecords.put(index, newRecord);
+                    System.out.println("Changed!");
+                default:
+                    break;
+            }
+            System.out.println("Please key in your option:");
+            choice = s.nextInt();
+        }
+
+    }
+
+    public void removeMovieReord(int index) {
+        movieRecords.remove(index);
+        System.out.println("Successfullly removed!");
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByMovieName(String moviename) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getMovieName() == moviename)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByMovieName(ArrayList<movieRecord> searchSpace, String moviename) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getMovieName() == moviename)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByMovieID(int movieID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getMovieID() == movieID)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByMovieID(ArrayList<movieRecord> searchSpace, int movieID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getMovieID() == movieID)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+
+    public ArrayList<movieRecord> findAllRecordsByCinemaID(int cinemaID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getCinemaID() == cinemaID)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByCinemaID(ArrayList<movieRecord> searchSpace, int cinemaID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getCinemaID() == cinemaID)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByCineplexID(int cineplexID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getCineplexID() == cineplexID)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByCineplexID(ArrayList<movieRecord> searchSpace, int cineplexID) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getCineplexID() == cineplexID)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByDate(String date) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getDateTime() == date)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByDate(ArrayList<movieRecord> searchSpace, String date) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getDateTime() == date)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByStarttime(String starttime) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getStartTime() == starttime)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByStarttime(ArrayList<movieRecord> searchSpace, String starttime) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getStartTime() == starttime)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByEndtime(String endtime) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+        for (int i = 0; i < movieRecords.size(); i++) {
+            if (movieRecords.get(i).getEndTime() == endtime)
+                records.add(movieRecords.get(i));
+        }
+        return records;
+    }
+
+    public ArrayList<movieRecord> findAllRecordsByEndtime(ArrayList<movieRecord> searchSpace, String endtime) {
+        ArrayList<movieRecord> records = new ArrayList<movieRecord>();
+
+        for (int i = 0; i < searchSpace.size(); i++) {
+            if (searchSpace.get(i).getEndTime() == endtime)
+                records.add(searchSpace.get(i));
+        }
+        return records;
+    }
+
+
+}
---
+++ Classes/StaffRecord.java
@@ -0,0 +1,134 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class StaffRecord {
+    // key: StaffID, value: password
+    private Map<String, String> staffIDPasswordRecord = new HashMap<String, String>();
+
+    /**
+    Default constructor with empty staffIDPasswordRecord
+     */
+    public StaffRecord() {}
+
+    /**
+     Initiate the class by adding a single staff record
+     */
+    public StaffRecord(String staffID, String password)
+    {
+        addLoginRecord(staffID, password);
+    }
+
+    /**
+     Initiate the class using a txt file to populate userPasswordRecord
+     */
+    public StaffRecord(String fileName)
+    {
+        this.addLoginRecords(fileName);
+    }
+
+    /**
+     Enable adding of a new staff record;
+     new staffID must be different than existing usernames; usernam
+     password
+     */
+    public void addLoginRecord(String staffID, String password)
+    {
+       try { // check whether this staffID exists or not;
+           if (staffIDPasswordRecord.containsKey(staffID))
+               throw new IllegalArgumentException();
+
+           // if does not exist:
+           this.staffIDPasswordRecord.put(staffID, password);
+       }
+       catch(IllegalArgumentException e)
+       {
+           System.out.println( "This staffID already exists! Try again!");
+           System.exit( 0 );
+       }
+    }
+
+
+    /**
+     Given a buffererReader stream, populate this.staffIDPasswordRecord
+     */
+    public void addLoginRecords(String fileName)
+    {
+        try {
+            FileReader frStream = new FileReader(fileName);
+            BufferedReader brStream = new BufferedReader(frStream);
+            String inputLine;
+            while((inputLine = brStream.readLine())!=null)
+            {
+
+                String delims = "[ ]+";
+                String[] tokens = inputLine.split(delims);
+                this.addLoginRecord(tokens[0], tokens[1]);
+            }
+            brStream.close();
+        }
+        catch ( FileNotFoundException e ) {
+            System.out.println( "Error opening the input file!");
+            System.exit( 0 );
+        }
+        catch ( IOException e ) {
+            System.out.println( "IO Error!");
+            e.printStackTrace();
+            System.exit( 0 );
+        }
+    }
+
+    /**
+     Prompts staff to enter staffID and username;
+     If all inputs are correct, login is successful;
+     Password is masked by asterisks;
+     returns true if login is successful.
+     Only runs in console! not in IDE!
+     */
+    public boolean login()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your staff ID:");
+        String id = s.nextLine();
+        Console console = System.console();
+        char[] passwordArray = console.readPassword("Please enter your password: ");
+        String password = new String(passwordArray);
+        // if running in IDE:
+        //        System.out.println("Please enter your passward:");
+        //        String password = s.nextLine();
+        if(!this.staffIDPasswordRecord.containsKey(id))
+        {
+            if(!this.staffIDPasswordRecord.get(id).equals(password)) {
+                System.out.println("Successful Login!");
+                return true;
+            }
+            else
+            {
+                System.out.println( "Wrong password! Try again!   ");
+                return false;
+            }
+        }
+        else
+        {
+            System.out.println( "This staffID does not exists! Try again!   ");
+            return false;
+        }
+    }
+
+
+    /**
+     Unit testing
+     */
+    public static void main(String args[])
+    {
+
+        //StaffRecord record = new StaffRecord("sazt", "123456");
+        StaffRecord record = new StaffRecord("staffRecords.txt");
+        record.login();
+
+    }
+
+
+}
