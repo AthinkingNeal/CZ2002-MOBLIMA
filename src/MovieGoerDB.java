76f6399fd3c05e6ebe7e94c9ece4994f61d2acdd
Neal <athinkingneal@gmail.com>
2019-11-11 19:38:32 +0800

    test movieGoer updateRecord

--- src/MovieGoerDB.java
+++ src/MovieGoerDB.java
@@ -50,17 +50,35 @@ public class MovieGoerDB implements Database {
         addRecord(name, movieGoerID, mobileNumber, emailAddress, age);
     }
 
+
+    public void addRecord(int movieGoerID) {
+        String dummy;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your name: ");
+        String name = sc.nextLine();
+        System.out.println("Please input your mobile number: ");
+        int mobileNumber = sc.nextInt();
+        dummy = sc.nextLine();
+        System.out.println("Please input your email address: ");
+        String emailAddress = sc.nextLine();
+        System.out.println("Please input your age: ");
+        int age = sc.nextInt();
+        dummy = sc.nextLine();
+
+        addRecord(name, movieGoerID, mobileNumber, emailAddress, age);
+    }
+
     // We assume that Movie Goer does not need to delete his/her own record
 
     public MovieGoer findRecordByMovieGoerID(int movieGoerID){
         for (Map.Entry<Integer,MovieGoer> entry : MovieGoerMap.entrySet()){
             if(entry.getValue().getMovieGoerID() == movieGoerID){
-                System.out.println("The movie goer is found!");
+                System.out.println(entry.getValue().getName() + " Welcome Back!");
                 return entry.getValue();
             }
         }
-        System.out.println("The movieGoer you are looking for does not exist, we are assigning you to this new ID");
-        addRecord();
+        System.out.println("The movieID you entered does not exist, we are assigning you to this new ID");
+        addRecord(movieGoerID);
 
         for (Map.Entry<Integer,MovieGoer> entry : MovieGoerMap.entrySet()){
             if(entry.getValue().getMovieGoerID() == movieGoerID){
@@ -76,6 +94,10 @@ public class MovieGoerDB implements Database {
         System.out.println("delete record");
     }
 
+    public void updateRecord(){
+        System.out.println("Update Record");
+    }
+
     public void updateRecord(int movieGoerID) {
         MovieGoer m = findRecordByMovieGoerID(movieGoerID);
         System.out.println("Which attribute would you like to update?");
@@ -86,27 +108,36 @@ public class MovieGoerDB implements Database {
         System.out.println("5. Exit");
 
         Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
+        int choice = Integer.parseInt(sc.nextLine());
+        // int choice = sc.nextInt();  this line seems to generate problem, the remaining \n character will be fed into sc.nextLine() below
         while (choice != 5) {
             switch (choice) {
                 case 1:
+                    System.out.println("Your current name is " + m.getName());
                     System.out.println("Please input your new name: ");
-                    m.setName(sc.nextLine());
+                    String temp = sc.nextLine();
+                    m.setName(temp);
+                    //TODO m.setName(sc.nextLine()); this line of code will execute the print message below first, why? Perhaps Because of the sc.nextInt() above.
                     break;
                 case 2:
+                    System.out.println("Your current mobile number is " + m.getMobileNumber());
                     System.out.println("Please input your new mobile number: ");
                     m.setMobileNumber(Integer.parseInt(sc.nextLine()));
                     break;
                 case 3:
+                    System.out.println("Your current email address is " + m.getEmailAddress());
                     System.out.println("Please input your new email address: ");
                     m.setEmailAddress(sc.nextLine());
                     break;
                 case 4:
+                    System.out.println("Your current age is " + m.getAge());
                     System.out.println("Please input your new age: ");
                     m.setAge(Integer.parseInt(sc.nextLine()));
                     break;
             }
-            System.out.println("Which attribute would you like to update?");
+            System.out.println("You have succesfully update your information!");
+
+            System.out.println("Which else attribute would you like to update?");
             System.out.println("1. Update name");
             System.out.println("2. Update mobile number");
             System.out.println("3. Update email address");
@@ -115,9 +146,7 @@ public class MovieGoerDB implements Database {
             choice = Integer.parseInt(sc.nextLine());
         }
 
-
-        System.out.println("Please input your age: ");
-        int age = sc.nextInt();
+        System.out.println("You are existing now...");
     }
 
 
